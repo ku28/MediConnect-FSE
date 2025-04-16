628bebe094591c64092ac4ab05685dbb56734533
ku28 <ku28sh07@gmail.com>
2025-04-16 18:05:41 +0530

    update email verification

--- src/components/Booking/BookingInvoice/BookingInvoice.jsx
+++ src/components/Booking/BookingInvoice/BookingInvoice.jsx
@@ -125,7 +125,7 @@ const BookingInvoice = () => {
                     </div>
                     <div className="other-info">
                         <h4>Other information</h4>
-                        <p className="text-muted mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed dictum ligula, cursus blandit risus. Maecenas eget metus non tellus dignissim aliquam ut a ex. Maecenas sed vehicula dui, ac suscipit lacus. Sed finibus leo vitae lorem interdum, eu scelerisque tellus fermentum. Curabitur sit amet lacinia lorem. Nullam finibus pellentesque libero.</p>
+                        <p className="text-muted mb-0"></p>
                     </div>
 
                 </div>
